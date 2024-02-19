737441b01b0b805928a95d934595fe3e4a2646d4
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-02-19 09:38:06 +0530

    6th solution added

--- pattern.js
+++ pattern.js
@@ -22,3 +22,55 @@ function pattern2(n){
 }
 
 pattern2(5);
+
+//pattern 03
+
+function pattern3(n){
+    for(let i=1; i<=n; i++){
+        for(let j=1; j<=i; j++){
+            process.stdout.write(j.toString()+ "");
+        }
+        console.log();
+    }
+}
+pattern3(5);
+
+
+//pattern 04
+
+function pattern4(n){
+    for(let i=1;i<=n; i++){
+        for(let j=1;j<=i; j++){
+            process.stdout.write(i +"");
+        }
+        console.log();
+    }
+}
+
+pattern4(5);
+
+
+//pattern 05 
+
+function pattern5(n){
+    for (let i=0; i<n; i++){
+        for(let j=n; j>i; j--){
+            process.stdout.write("*");
+        }
+        console.log();
+    }
+}
+pattern5(5);
+
+//pattern 06 
+
+function pattern6(n){
+    for(let i=1; i<=n; i++){
+        for(let j=n; j>=i; j--){
+            process.stdout.write(n-j+1 + "");
+        }
+        console.log();
+    }
+}
+
+pattern6(5);
