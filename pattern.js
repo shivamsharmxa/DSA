c2c6f514de2ad9354c549a5ae34ea555e0a39324
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-02-19 08:19:48 +0530

    add file

---
+++ pattern.js
@@ -0,0 +1,14 @@
+//pattern 01
+function pattern1(n){
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+            process.stdout.write("* ");
+        }
+        console.log();
+    }
+    
+}
+pattern1(5);
+
+//pattern 02 
+
