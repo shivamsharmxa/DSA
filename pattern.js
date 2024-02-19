52c4e1f7a54037d13ec399fbfbb2fd98d0b1bd49
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-02-19 08:32:18 +0530

    second solution

--- pattern.js
+++ pattern.js
@@ -12,3 +12,13 @@ pattern1(5);
 
 //pattern 02 
 
+function pattern2(n){
+    for(let i=0; i<n; i++){
+        for(let j=0 ; j<=i; j++){
+            process.stdout.write("*");
+        }
+        console.log();
+    }
+}
+
+pattern2(5);
