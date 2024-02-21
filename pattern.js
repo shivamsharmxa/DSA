3cc3f525032de1a42863c3f271de5705bb6aa3a7
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-02-21 11:25:23 +0530

    one more solution

--- pattern.js
+++ pattern.js
@@ -1,96 +1,100 @@
-//pattern 01
-function pattern1(n){
-    for(let i=0; i<n; i++){
-        for(let j=0; j<n; j++){
-            process.stdout.write("* ");
-        }
-        console.log();
-    }
+// //pattern 01
+// function pattern1(n){
+//     for(let i=0; i<n; i++){
+//         for(let j=0; j<n; j++){
+//             process.stdout.write("* ");
+//         }
+//         console.log();
+//     }
     
-}
-pattern1(5);
+// }
+// pattern1(5);
 
-//pattern 02 
+// //pattern 02 
 
-function pattern2(n){
-    for(let i=0; i<n; i++){
-        for(let j=0 ; j<=i; j++){
-            process.stdout.write("*");
-        }
-        console.log();
-    }
-}
+// function pattern2(n){
+//     for(let i=0; i<n; i++){
+//         for(let j=0 ; j<=i; j++){
+//             process.stdout.write("*");
+//         }
+//         console.log();
+//     }
+// }
 
-pattern2(5);
+// pattern2(5);
 
-//pattern 03
+// //pattern 03
 
-function pattern3(n){
-    for(let i=1; i<=n; i++){
-        for(let j=1; j<=i; j++){
-            process.stdout.write(j.toString()+ "");
-        }
-        console.log();
-    }
-}
-pattern3(5);
-
-
-//pattern 04
-
-function pattern4(n){
-    for(let i=1;i<=n; i++){
-        for(let j=1;j<=i; j++){
-            process.stdout.write(i +"");
-        }
-        console.log();
-    }
-}
-
-pattern4(5);
+// function pattern3(n){
+//     for(let i=1; i<=n; i++){
+//         for(let j=1; j<=i; j++){
+//             process.stdout.write(j.toString()+ "");
+//         }
+//         console.log();
+//     }
+// }
+// pattern3(5);
 
 
-//pattern 05 
+// //pattern 04
 
-function pattern5(n){
-    for (let i=0; i<n; i++){
-        for(let j=n; j>i; j--){
-            process.stdout.write("*");
-        }
-        console.log();
-    }
-}
-pattern5(5);
+// function pattern4(n){
+//     for(let i=1;i<=n; i++){
+//         for(let j=1;j<=i; j++){
+//             process.stdout.write(i +"");
+//         }
+//         console.log();
+//     }
+// }
 
-//pattern 06 
+// pattern4(5);
 
-function pattern6(n){
-    for(let i=1; i<=n; i++){
-        for(let j=n; j>=i; j--){
-            process.stdout.write(n-j+1 + "");
-        }
-        console.log();
-    }
-}
 
-pattern6(5);
+// //pattern 05 
+
+// function pattern5(n){
+//     for (let i=0; i<n; i++){
+//         for(let j=n; j>i; j--){
+//             process.stdout.write("*");
+//         }
+//         console.log();
+//     }
+// }
+// pattern5(5);
+
+// //pattern 06 
+
+// function pattern6(n){
+//     for(let i=1; i<=n; i++){
+//         for(let j=n; j>=i; j--){
+//             process.stdout.write(n-j+1 + "");
+//         }
+//         console.log();
+//     }
+// }
+
+// pattern6(5);
 
 //pattern 07 
+
+function spacePrinter(start, end){
+    for (let j = start; j < end; j++) {
+        process.stdout.write(" ");
+    }
+}
 
 function pattern7(n) {
     for (let i = 0; i < n; i++) {
         // space
-        for (let j = 0; j < n - i - 1; j++) {
-            process.stdout.write(" ");
-        }
+        let space = n - i - 1;
+        let star = 2 * i;
+        spacePrinter(0, space)
         // star
-        for (let j = 0; j <= 2 * i; j++) {
+        for (let j = 0; j <= star; j++) {
             process.stdout.write("*");
         }
         // space
-        for (let j = 0; j < n - i - 1; j++) {
-            process.stdout.write(" ");
-        }
+        spacePrinter(0, space)
         console.log(); // Move to the next line after each row
     }
 }
@@ -100,61 +104,87 @@ pattern7(5);
 
 //pattern 08
 
-function pattern8(n){
-    for(let i=0; i<n; i++){
+ function pattern8(n){
+     for(let i=0; i<n; i++){
         //for space
 
         for(let j =0; j<i; j++){
-            process.stdout.write(" ");
-        }
+             process.stdout.write(" ");
+         }
 
-        //stars
-        for(let j=0; j< 2*n -(2*i+1); j++){
-            process.stdout.write("*");
-        }
+         //stars
+         for(let j=0; j< 2*n -(2*i+1); j++){
+             process.stdout.write("*");
+         }
 
-        for(let j=0; j<i; j++){
+         for(let j=0; j<i; j++){
             process.stdout.write(" ");
-        }
-        console.log();
-    }
-}
+         }
+         console.log();
+     }
+ }
 pattern8(5);
 
-//pattern 09
+  //pattern 09
 
-function pattern9(n){
-    for(let i=0; i<n; i++){
+ function pattern9(n){
+     for(let i=0; i<n; i++){
         //space
-        for (let j = 0; j < n - i - 1; j++) {
-            process.stdout.write(" ");
-        }
-        // star
-        for (let j = 0; j <= 2 * i; j++) {
+         for (let j = 0; j < n - i - 1; j++) {
+             process.stdout.write(" ");
+         }
+         // star
+         for (let j = 0; j <= 2 * i; j++) {
+             process.stdout.write("*");
+         }
+         // space
+         for (let j = 0; j < n - i - 1; j++) {
+             process.stdout.write(" ");
+         }
+         console.log();
+     }
+     for(let i=0; i<n; i++){
+         //for space
+
+         for(let j =0; j<i; j++){
+             process.stdout.write(" ");
+         }
+
+         //stars
+         for(let j=0; j< 2*n -(2*i+1); j++){
+             process.stdout.write("*");
+         }
+
+         for(let j=0; j<i; j++){
+             process.stdout.write(" ");
+         }
+         console.log();
+     }
+ }
+ pattern9(5);
+
+ //pattter 10
+
+ function pattern10(n) {
+   
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= i; j++) {
             process.stdout.write("*");
-        }
-        // space
-        for (let j = 0; j < n - i - 1; j++) {
-            process.stdout.write(" ");
         }
         console.log();
     }
-    for(let i=0; i<n; i++){
-        //for space
 
-        for(let j =0; j<i; j++){
-            process.stdout.write(" ");
-        }
-
-        //stars
-        for(let j=0; j< 2*n -(2*i+1); j++){
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i; j++) {
             process.stdout.write("*");
-        }
-
-        for(let j=0; j<i; j++){
-            process.stdout.write(" ");
         }
         console.log();
     }
 }
-pattern9(5);
+
+
+pattern10(6);
+
+ 
+
