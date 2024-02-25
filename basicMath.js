1a1749d4d42cb315a3835b5fbb586c99e5625ec2
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-02-25 07:40:14 +0530

    reverse Number and Palindrome added

--- basicMath.js
+++ basicMath.js
@@ -8,4 +8,43 @@ function countdigit(n){
     process.stdout.write(count.toString());
 }
 
-countdigit(325);
+// countdigit(325);
+
+//reverse a number
+function Reverse(N){
+    let reverseNum = 0;
+    let lastdigit = 0;
+    while(N > 0){
+        lastdigit = N % 10; //last digit
+        N = Math.floor(N / 10);
+
+        reverseNum = (reverseNum * 10) + lastdigit; 
+    }
+    console.log(reverseNum);
+}
+
+// Reverse(1234);
+
+
+//palindrome Number
+
+function palindrome(N){
+    let reverseNum = 0;
+    let originalNum = N;
+    let lastdigit;
+
+    while(N > 0){
+        lastdigit = N % 10;
+        N = Math.floor(N / 10);
+
+        reverseNum = (reverseNum * 10) + lastdigit;
+    }
+    
+    if(reverseNum === originalNum){
+        console.log("It's a palindrome Number");
+    } else {
+        console.log("It's not a palindrome number");
+    }
+}
+
+palindrome(414);
