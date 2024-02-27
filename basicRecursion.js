69ae65b6a6dc9aaad9bc002330726f58eadf7ce0
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-02-27 07:51:25 +0530

    reverse the array

--- basicRecursion.js
+++ basicRecursion.js
@@ -61,3 +61,47 @@ function printingSum(n) {
 }
 
 printingSum(5);
+
+
+//factorial of n numbers
+
+function factorial(n){
+    //base condition
+    if(n==0){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+function printFactorial(n){
+    console.log(factorial(n));
+}
+
+printFactorial(5);
+
+// reverse the array 
+
+function printArray(arr) {
+    console.log("The reversed array is:-");
+    for (let i = 0; i < arr.length; i++) {
+        process.stdout.write(arr[i] + " ");
+    }
+    console.log();
+}
+//function to reverse the array 
+function reversedArray(arr,start,end){
+    if(start<end){
+        [arr[start],arr[end] = arr[end],arr[start]];
+        reversedArray(arr,start + 1,end - 1);
+
+    }
+}
+
+function main(){
+    const n =5;
+    const arr=[10,9,8,7,6];
+    reversedArray(arr,0,n-1);
+    printArray(arr);
+}
+
+main();
