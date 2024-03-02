edccbc7f2abd46c508491d4fc1a5c0303a97b9b2
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-03-02 23:37:54 +0530

    second problem

--- basicHashing.js
+++ basicHashing.js
@@ -17,5 +17,42 @@ function frequency(arr) {
 }
 
 // Example array
+// let arr = [10, 5, 10, 15, 10, 5];
+// frequency(arr);
+
+
+function frequency(arr) {
+    let map = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+        let key = arr[i];
+        if (map.has(key)) {
+            map.set(key, map.get(key) + 1);
+        } else {
+            map.set(key, 1);
+        }
+    }
+
+    let maxFreq = 0, minFreq = arr.length;
+    let maxEle = 0, minEle = 0;
+
+    // Traverse through the map to find the elements.
+    map.forEach((count, element) => {
+        if (count > maxFreq) {
+            maxEle = element;
+            maxFreq = count;
+        }
+        if (count < minFreq) {
+            minEle = element;
+            minFreq = count;
+        }
+    });
+
+    console.log("The highest frequency element is: " + maxEle);
+    console.log("The lowest frequency element is: " + minEle);
+}
+
+// Example array
 let arr = [10, 5, 10, 15, 10, 5];
 frequency(arr);
+
