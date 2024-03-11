e7e97aee6c3ea06c8af860f8ccf8bc9fef0d5305
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-03-11 07:47:33 +0530

    Numbers divisible by 2 and 3 and their frequencies

--- basicHashing.js
+++ basicHashing.js
@@ -1,101 +1,80 @@
-function frequency(arr) {
-    let map = new Map();
+// function frequency(arr) {
+//     let map = new Map();
 
+//     for (let i = 0; i < arr.length; i++) {
+//         let key = arr[i];
+//         if (map.has(key)) {
+//             map.set(key, map.get(key) + 1);
+//         } else {
+//             map.set(key, 1);
+//         }
+//     }
+
+//     // Traverse through the map and print frequencies
+//     map.forEach((value, key) => {
+//         console.log(`${key} ${value}`);
+//     });
+// }
+
+// // Example array
+// // let arr = [10, 5, 10, 15, 10, 5];
+// // frequency(arr);
+
+
+// function frequency(arr) {
+//     let map = new Map();
+
+//     for (let i = 0; i < arr.length; i++) {
+//         let key = arr[i];
+//         if (map.has(key)) {
+//             map.set(key, map.get(key) + 1);
+//         } else {
+//             map.set(key, 1);
+//         }
+//     }
+
+//     let maxFreq = 0, minFreq = arr.length;
+//     let maxEle = 0, minEle = 0;
+
+//     // Traverse through the map to find the elements.
+//     map.forEach((count, element) => {
+//         if (count > maxFreq) {
+//             maxEle = element;
+//             maxFreq = count;
+//         }
+//         if (count < minFreq) {
+//             minEle = element;
+//             minFreq = count;
+//         }
+//     });
+
+//     console.log("The highest frequency element is: " + maxEle);
+//     console.log("The lowest frequency element is: " + minEle);
+// }
+
+//frequency of number which is divisible by 2 and 3 
+function findDivisibleNumbers(arr) {
+    const frequencies = { divisibleBy2: 0, divisibleBy3: 0 };
+  
     for (let i = 0; i < arr.length; i++) {
-        let key = arr[i];
-        if (map.has(key)) {
-            map.set(key, map.get(key) + 1);
-        } else {
-            map.set(key, 1);
-        }
+      const number = arr[i];
+  
+      if (number % 2 === 0) {
+        frequencies.divisibleBy2++;
+      }
+  
+      if (number % 3 === 0) {
+        frequencies.divisibleBy3++;
+      }
     }
-
-    // Traverse through the map and print frequencies
-    map.forEach((value, key) => {
-        console.log(`${key} ${value}`);
-    });
-}
-
-// Example array
-// let arr = [10, 5, 10, 15, 10, 5];
-// frequency(arr);
-
-
-function frequency(arr) {
-    let map = new Map();
-
-    for (let i = 0; i < arr.length; i++) {
-        let key = arr[i];
-        if (map.has(key)) {
-            map.set(key, map.get(key) + 1);
-        } else {
-            map.set(key, 1);
-        }
-    }
-
-    let maxFreq = 0, minFreq = arr.length;
-    let maxEle = 0, minEle = 0;
-
-    // Traverse through the map to find the elements.
-    map.forEach((count, element) => {
-        if (count > maxFreq) {
-            maxEle = element;
-            maxFreq = count;
-        }
-        if (count < minFreq) {
-            minEle = element;
-            minFreq = count;
-        }
-    });
-
-    console.log("The highest frequency element is: " + maxEle);
-    console.log("The lowest frequency element is: " + minEle);
-}
-
-// Example array
-
-//Map()
-
-let arrOne = [12,13,14,15,16];
-const multiFive = (num)=>{
-    return num * 2; 
-}
-let arrTwo = arrOne.map(multiFive);
-console.log("Changed array is : ",arrTwo);
-console.log("Previous array is : ",arrOne);
-
-//Filter()
-
-let array1 = [15,34,56,21,18];
-const divbythree = (num)=>{
-    return num%3 == 0
-}
-
-let array2 = array1.filter(divbythree);
-console.log("number divided by 3 is : ",array2);
-
-// promise chain
-
- getData(1).then((res)=>{console.log(res);
-    getData(2).then((res)=>{
-        console.log(res)
-    })
-})
-
-//callback hell 
-getData(1,()=>{
-    console.log("data2 is......")
-    getData(2,()=>{
-        console.log("data3 is......")
-        getData(3,()=>{
-            console.log("data4 is.....")
-            getData(4)
-        })
-    })
-});
-
-
-Map()
-
-
-
+  
+    return frequencies;
+  }
+  
+  const numbers = [2, 3, 4, 6, 9, 12, 14, 18, 21, 4, 6, 12, 18];
+  
+  const result = findDivisibleNumbers(numbers);
+  
+  console.log('Numbers divisible by 2 and their frequencies:', result.divisibleBy2);
+  console.log('Numbers divisible by 3 and their frequencies:', result.divisibleBy3);
+  
