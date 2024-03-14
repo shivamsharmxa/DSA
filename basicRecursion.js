8d9b47a63dfde6ed802fd85908d522e7dcae8282
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-03-14 10:32:12 +0530

    Another way to solve

--- basicRecursion.js
+++ basicRecursion.js
@@ -1,143 +1,159 @@
-//printing n times
-let count = 0;
-function ntimes(){
-    if(count===5) return;
-    console.log(count);
-    count++;
-    ntimes();
-}
+// //printing n times
+// let count = 0;
+// function ntimes(){
+//     if(count===5) return;
+//     console.log(count);
+//     count++;
+//     ntimes();
+// }
 
-function main(){
-    ntimes();
-}
+// function main(){
+//     ntimes();
+// }
+
+// // main();
+
+// //printing name n times
+
+// function func(i, n) {
+//     // Base Condition.
+//     if (i > n) return;
+//     console.log("Shivam");
+ 
+   
+//     func(i + 1, n);
+// }
+
+// function name() {
+//     func(1, 5);
+// }
+
+
+// // name();
+
+
+// //printing from N to i
+
+// function func(i,n){
+//     //base condition
+//     if(i<1) return;
+//     console.log(i);
+
+//     func(i-1,n);
+// }
+
+// function decrement(i,n){
+//     func(n,n);
+//     return ;
+// }
+// decrement(5,5);
+
+
+// //sum of first N numbers
+
+// function sum(n) {
+//     let sum = (n * (n + 1)) / 2;
+//     console.log(sum);
+// }
+
+// function printingSum(n) {
+//     sum(n);
+// }
+
+// printingSum(5);
+
+
+// //factorial of n numbers
+
+// function factorial(n){
+//     //base condition
+//     if(n==0){
+//         return 1;
+//     }
+//     return n*factorial(n-1);
+// }
+
+// function printFactorial(n){
+//     console.log(factorial(n));
+// }
+
+// printFactorial(5);
+
+// // reverse the array 
+
+// function printArray(arr) {
+//     console.log("The reversed array is:-");
+//     for (let i = 0; i < arr.length; i++) {
+//         process.stdout.write(arr[i] + " ");
+//     }
+//     console.log();
+// }
+// //function to reverse the array 
+// function reversedArray(arr,start,end){
+//     if(start<end){
+//         [arr[start],arr[end] = arr[end],arr[start]];
+//         reversedArray(arr,start + 1,end - 1);
+
+//     }
+// }
+
+// function main(){
+//     const n =5;
+//     const arr=[10,9,8,7,6];
+//     reversedArray(arr,0,n-1);
+//     printArray(arr);
+// }
 
 // main();
 
-//printing name n times
+// //check if the given string is palindrome or not
 
-function func(i, n) {
-    // Base Condition.
-    if (i > n) return;
-    console.log("Shivam");
- 
-   
-    func(i + 1, n);
-}
+// function palindrome(i, s) {
+//     if (i >= s.length / 2) return true;
 
-function name() {
-    func(1, 5);
-}
+//     if (s[i] !== s[s.length - i - 1]) return false;
 
+//     return palindrome(i + 1, s);
+// }
 
-// name();
+// function checkPalindrome(i, s) {
+//     return palindrome(i, s);
+// }
 
-
-//printing from N to i
-
-function func(i,n){
-    //base condition
-    if(i<1) return;
-    console.log(i);
-
-    func(i-1,n);
-}
-
-function decrement(i,n){
-    func(n,n);
-    return ;
-}
-decrement(5,5);
+// // Example string.
+// let naman = "naman";
+// console.log(checkPalindrome(0, naman));
 
 
-//sum of first N numbers
+// //fibonacci Number
+// function fibonacci(N){
+//     //base condition 
+//     if(N<=1) return N;
 
-function sum(n) {
-    let sum = (n * (n + 1)) / 2;
-    console.log(sum);
-}
+//     let last = fibonacci(N-1);
+//     let prevLast = fibonacci(N-2);
 
-function printingSum(n) {
-    sum(n);
-}
+//     return last + prevLast;
+// }
 
-printingSum(5);
+// function printFibonacci(N){
+//     return fibonacci(N)
+
+// }
+// console.log(printFibonacci(4));
 
 
-//factorial of n numbers
-
-function factorial(n){
-    //base condition
-    if(n==0){
-        return 1;
-    }
-    return n*factorial(n-1);
-}
-
-function printFactorial(n){
-    console.log(factorial(n));
-}
-
-printFactorial(5);
-
-// reverse the array 
-
-function printArray(arr) {
-    console.log("The reversed array is:-");
-    for (let i = 0; i < arr.length; i++) {
-        process.stdout.write(arr[i] + " ");
-    }
-    console.log();
-}
-//function to reverse the array 
-function reversedArray(arr,start,end){
-    if(start<end){
-        [arr[start],arr[end] = arr[end],arr[start]];
-        reversedArray(arr,start + 1,end - 1);
-
-    }
+//Printing 1 to N times
+function print(N){
+    //base Condition
+  if(N==0) return;
+  print(N-1);
+  console.log(N);
+  return;
 }
 
 function main(){
-    const n =5;
-    const arr=[10,9,8,7,6];
-    reversedArray(arr,0,n-1);
-    printArray(arr);
+    print(5);
 }
-
 main();
 
-//check if the given string is palindrome or not
-
-function palindrome(i, s) {
-    if (i >= s.length / 2) return true;
-
-    if (s[i] !== s[s.length - i - 1]) return false;
-
-    return palindrome(i + 1, s);
-}
-
-function checkPalindrome(i, s) {
-    return palindrome(i, s);
-}
-
-// Example string.
-let naman = "naman";
-console.log(checkPalindrome(0, naman));
-
-
-//fibonacci Number
-function fibonacci(N){
-    //base condition 
-    if(N<=1) return N;
-
-    let last = fibonacci(N-1);
-    let prevLast = fibonacci(N-2);
-
-    return last + prevLast;
-}
-
-function printFibonacci(N){
-    return fibonacci(N)
-
-}
-console.log(printFibonacci(4));
